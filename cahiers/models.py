a1aac190f6db61890f28e5dc7df114af40ac4adb
marcwebbie <marcwebbie@gmail.com>
2015-04-26 17:50:07 -0300

    Update cahiers models

--- cahiers/models.py
+++ cahiers/models.py
@@ -2,69 +2,71 @@ from uuid import uuid4
 
 from django.db import models
 
+from accounts.models import Profile
+
 
 class Cahier(models.Model):
+    profile = models.ForeignKey(Profile)
     description = models.CharField(max_length=255)
     budget = models.FloatField(max_length=255)
 
     def __str__(self):
         return self.description
 
-class Pays(models.Model):
-    nom = models.CharField(max_length=255)
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
 
     class Meta:
-        verbose_name_plural = 'Pays'
+        verbose_name_plural = 'Countries'
 
     def __str__(self):
-        return self.nom
+        return self.name
 
 
-class Ville(models.Model):
-    nom = models.CharField(max_length=255)
-    pays = models.ForeignKey(Pays)
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(Country)
 
     def __str__(self):
-        return self.nom
+        return self.name
 
 
 class Logement(models.Model):
-    uuid = models.UUIDField(default=uuid4, editable=False, primary_key=True)
     cahier = models.ForeignKey(Cahier, related_name='logements')
 
     # Geo
-    ville = models.ForeignKey(Ville)
-    quartier = models.CharField(max_length=255, null=True, blank=True)
-    code_postal = models.CharField(max_length=255, null=True, blank=True)
+    city = models.ForeignKey(City)
+    area = models.CharField(max_length=255, null=True, blank=True)
+    postal_code = models.CharField(max_length=255, null=True, blank=True)
 
     # Finances
-    prix = models.FloatField()
-    garantie = models.FloatField(default=0, null=False, blank=False)
+    price = models.FloatField()
+    warrant = models.FloatField(default=0, null=False, blank=False)
     caution = models.FloatField(default=0, null=False, blank=False)
     charges = models.FloatField(default=0, null=False, blank=False)
 
     # Espace
     surface = models.IntegerField()
-    etage = models.IntegerField(null=True, blank=True)
-    num_pieces = models.IntegerField(default=1)
-    num_chambres = models.IntegerField(default=0)
-    ascenseur = models.IntegerField()
-    num_places_parking = models.IntegerField()
+    floor = models.IntegerField(null=True, blank=True)
+    num_room = models.IntegerField(default=1)
+    num_bedroom = models.IntegerField(default=0)
+    elevator = models.IntegerField()
+    num_parking = models.IntegerField()
 
     # Atouts
     climatisation = models.BooleanField()
-    purificateur_eau = models.BooleanField()
+    water_purifier = models.BooleanField()
     balcon = models.BooleanField()
-    jardin = models.BooleanField()
+    garden = models.BooleanField()
 
     # Immeuble
     gardien = models.BooleanField()
-    piscine = models.BooleanField()
+    swimming_pool = models.BooleanField()
     playground = models.BooleanField()
     gym = models.BooleanField()
     spa = models.BooleanField()
-    espace_enfant = models.BooleanField()
-    espace_barbecue = models.BooleanField()
+    barbecue_area = models.BooleanField()
 
     # Resourses
     url = models.URLField()
@@ -74,4 +76,9 @@ class Logement(models.Model):
     description = models.TextField()
 
     def __str__(self):
-        return "{}, {}".format(self.ville, self.prix)
+        return "{}, {}m2, Price: {}".format(self.city, self.surface, self.price)
+
+
+class LogementPhoto(models.Model):
+    logement = models.ForeignKey(Logement)
+    description = models.CharField(max_length=255, null=True, blank=True)
