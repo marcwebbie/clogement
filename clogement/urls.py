f82e6931e93a1c115eeb39fbe38e93f954f1afad
marcwebbie <marcwebbie@gmail.com>
2015-04-26 17:51:39 -0300

    Update clogement urls

--- clogement/urls.py
+++ clogement/urls.py
@@ -1,25 +1,12 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
-from tastypie.api import Api
-from cahiers.api import (
-    CahierResource,
-    PaysResource,
-    LogementResource,
-    VilleResource
-)
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'calog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
-
-v1_api = Api(api_name='v1')
-v1_api.register(CahierResource())
-v1_api.register(PaysResource())
-v1_api.register(VilleResource())
-v1_api.register(LogementResource())
-
-
-urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    (r'^api/', include(v1_api.urls)),
-)
+    url(r'^api/', include('cahiers.urls')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
+]
