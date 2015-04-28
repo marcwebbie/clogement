2211c462be4b3ec0361f83afba4ab20969313a3f
marcwebbie <marcwebbie@gmail.com>
2015-04-27 22:28:30 -0300

    Update urls

--- cahiers/urls.py
+++ cahiers/urls.py
@@ -4,9 +4,9 @@ from cahiers import views
 
 router = SimpleRouter()
 
-router.register(r'cahier', views.CahierViewSet, 'Cahier')
-router.register(r'country', views.CountryViewSet, 'Country')
-router.register(r'city', views.CityViewSet, 'City')
-router.register(r'logement', views.LogementViewSet, 'Logement')
+router.register(r'cahiers', views.CahierViewSet, 'Cahier')
+router.register(r'countries', views.CountryViewSet, 'Country')
+router.register(r'cities', views.CityViewSet, 'City')
+router.register(r'logements', views.LogementViewSet, 'Logement')
 
 urlpatterns = router.urls
